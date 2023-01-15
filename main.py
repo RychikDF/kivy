c2b1767e2d3fa1922ee871b6417e04709bb5f22a
Дима <https://github.com/RychikDF/Alumika.git>
2023-01-15 21:44:46 +0300

    11_commit Исправил чтобы правильно считалось, но теперь не работает разделитель чисел. version 1

--- main.py
+++ main.py
@@ -4,40 +4,42 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.window import Window
 import locale
+import collections
 #Window.size = (720, 1280) # Примерный экран смартфона
 Window.size = (480, 853)
 locale.setlocale(locale.LC_ALL, '')
 
 def get_calc(a, b, c):  # обработка результата
     if a > 1 and b > 1 and c >= 1:
-        window1 = str(((a*2) + (b*2)) * c)
-        window2 = str(c * 4)
-        window3 = str((c * 4) * 2)
+        window1 = (((a*2) + (b*2)) * c)
+        window2 = (c * 4)
+        window3 = ((c * 4) * 2)
     elif a > 1 and b > 1 and c < 1:
-        window1 = str((a * 2) + (b * 2))
-        window2, window3 = str("0"), str("0")
+        window1 = (a) + (b)
+        window2, window3 = (0), (0)
 
     elif a < 1 and b < 1 and c < 1:
-        window1, window2, window3 = str("0"), str("0"), str("0")
+        window1, window2, window3 = (0), (0), (0)
 
     elif a < 1 or b < 1 and c >= 1:
-        window1, window2, window3 = str("0"), str("0"), str("0")
+        window1, window2, window3 = (0), (0), (0)
 
     elif a < 1 or b < 1 and c < 1:
-        window1, window2, window3 = str("0"), str("0"), str("0")
+        window1, window2, window3 = (0), (0), (0)
 
 
     """Разделитель текста, 1 234 567 890 """
-    window1_a = locale.format_string('%d', int(window1), grouping=True)
+    window1 = locale.format_string('%d', int(window1), grouping=True)
 
 
-    return {"window1": window1_a, "window2": window2, "window3": window3}
+    return {"window1": window1, "window2": window2, "window3": window3}
+
 
 
 class Container(BoxLayout):
 
-    def sbor(self): #Сбор и отправка данных
 
+    def sbor(self): #Сбор и отправка данных
 
         try: window_a_1 = int(self.text_input_a1.text)
         except ValueError: window_a_1 = 0
@@ -135,19 +137,38 @@ class Container(BoxLayout):
             window_a_10, window_b_10 = 0, 0
 
 
-        tx1 = window_a_1 + window_a_2 + window_a_3 + window_a_4 + window_a_5 + \
-              window_a_6 + window_a_7 + window_a_8 + window_a_9 + window_a_10
-        tx2 = window_b_1 + window_b_2 + window_b_3 + window_b_4 + window_b_5 + \
-              window_b_6 + window_b_7 + window_b_8 + window_b_9 + window_b_10
-        tx3 = window_c_1 + window_c_2 + window_c_3 + window_c_4 + window_c_5 + \
-              window_c_6 + window_c_7 + window_c_8 + window_c_9 + window_c_10
+         #СОбераем списки
+        sbor_win1 = get_calc(window_a_1, window_b_1, window_c_1)
+        sbor_win2 = get_calc(window_a_2, window_b_2, window_c_2)
+        sbor_win3 = get_calc(window_a_3, window_b_3, window_c_3)
+        sbor_win4 = get_calc(window_a_4, window_b_4, window_c_4)
+        sbor_win5 = get_calc(window_a_5, window_b_5, window_c_5)
+        sbor_win6 = get_calc(window_a_6, window_b_6, window_c_6)
+        sbor_win7 = get_calc(window_a_7, window_b_7, window_c_7)
+        sbor_win8 = get_calc(window_a_8, window_b_8, window_c_8)
+        sbor_win9 = get_calc(window_a_9, window_b_9, window_c_9)
+        sbor_win10 = get_calc(window_a_10, window_b_10, window_c_10)
+         #создаем доп список
+        sbor_win = collections.Counter()
+         #загружаем все в один список
+        sbor_win.update(sbor_win1)
+        sbor_win.update(sbor_win2)
+        sbor_win.update(sbor_win3)
+        sbor_win.update(sbor_win4)
+        sbor_win.update(sbor_win5)
+        sbor_win.update(sbor_win6)
+        sbor_win.update(sbor_win7)
+        sbor_win.update(sbor_win8)
+        sbor_win.update(sbor_win9)
+        sbor_win.update(sbor_win10)
 
 
+        print(sbor_win)
 
-        sbor_win = get_calc(tx1,tx2,tx3)
-        self.window_l1.text = sbor_win.get("window1") + " мм"
-        self.window_l2.text = sbor_win.get("window2") + " шт."
-        self.window_l3.text = sbor_win.get("window3") + " шт."
+         #выводим на экран
+        self.window_l1.text = str(sbor_win.get("window1")) + " мм"
+        self.window_l2.text = str(sbor_win.get("window2")) + " ш.т."
+        self.window_l3.text = str(sbor_win.get("window3")) + " ш.т."
 
     def Clear(self): # Замена символов в полях
         self.window_l1.text = "мм"
